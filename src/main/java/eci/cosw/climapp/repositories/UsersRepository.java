bc8238e758de06f19b0378082553b40120cb9679
stefany288 <stefamoron25@gmail.com>
2018-03-27 14:44:54 -0500

    BDuPDATEuSER

--- src/main/java/eci/cosw/climapp/repositories/UsersRepository.java
+++ src/main/java/eci/cosw/climapp/repositories/UsersRepository.java
@@ -2,6 +2,7 @@ package eci.cosw.climapp.repositories;
 
 import eci.cosw.climapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +18,8 @@ public interface UsersRepository extends JpaRepository <User,Integer>{
     
     @Query(value ="select * from Users u where u.email = ?1 and u.password = ?2", nativeQuery = true)
     User findUserByEmailAndPassword(String email, String password);
+    
+    @Modifying
+    @Query(value = "update Users u set u.firstname = ?1 where u.lastname = ?2", nativeQuery = true )
+    User updateUser(User user, User u);
 }
