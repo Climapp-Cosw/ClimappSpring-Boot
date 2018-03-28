5b7a3065277d1b72a88c25f4e260d545bb747eb0
stefany288 <stefamoron25@gmail.com>
2018-03-28 13:35:20 -0500

    BDuPDATEuSER

--- src/main/java/eci/cosw/climapp/repositories/UsersRepository.java
+++ src/main/java/eci/cosw/climapp/repositories/UsersRepository.java
@@ -26,7 +26,6 @@ public interface UsersRepository extends JpaRepository <User,Integer>{
     @Modifying
     @Query(value = "update Users u set u.name = ?1, u.email = ?2, u.password = ?3, u.image = ?4  where u.id = ?5", nativeQuery = true )
     User updateUser(String name, String email, String password, String image, int id);
-	
 
 
 }
