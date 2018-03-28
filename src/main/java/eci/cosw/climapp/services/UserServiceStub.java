7cd63ac818e79ac19e76f2a1b38a8328170c047c
stefany288 <stefamoron25@gmail.com>
2018-03-28 10:56:04 -0500

    BDuPDATEuSER

--- src/main/java/eci/cosw/climapp/services/UserServiceStub.java
+++ src/main/java/eci/cosw/climapp/services/UserServiceStub.java
@@ -40,6 +40,11 @@ public class UserServiceStub implements UserService {
     }
     @Override
     public User updateUser(User user,User u){
+        u.setImage(user.getImage());
+        u.setPassword(user.getPassword());
+        u.setEmail(user.getEmail());
+        u.setName(user.getName());
+        u.setConfirmPassword(user.getConfirmPassword());
         return u;
     }
 
