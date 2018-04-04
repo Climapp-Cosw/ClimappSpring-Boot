35b00be9afb8e9d2f193eeac4fc472fd279aea71
MariaPaulaP <maria.pinzon-c@mail.escuelaing.edu.co>
2018-04-03 21:44:41 -0500

    arreglo

--- src/main/java/eci/cosw/climapp/repositories/UsersRepository.java
+++ src/main/java/eci/cosw/climapp/repositories/UsersRepository.java
@@ -2,11 +2,11 @@
 package eci.cosw.climapp.repositories;
 
 import eci.cosw.climapp.models.User;
-import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
